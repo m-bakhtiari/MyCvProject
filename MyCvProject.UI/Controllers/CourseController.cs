3e25bd70c1747d792a80c3d121811a7ee9705c1a
Mohammadreza <bakhtiarimohammadreza2@gmail.com>
2021-07-18 19:57:40 +0430

    Create project layers with clean architecture

---
+++ MyCvProject.UI/Controllers/CourseController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TopLearn.Core.Services.Interfaces;
+using TopLearn.DataLayer.Entities.Course;
+
+namespace TopLearn.Web.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+        private IOrderService _orderService;
+        private IUserService _userService;
+
+        public CourseController(ICourseService courseService, IOrderService orderService, IUserService userService)
+        {
+            _courseService = courseService;
+            _orderService = orderService;
+            _userService = userService;
+        }
+
+        public IActionResult Index(int pageId = 1, string filter = ""
+            , string getType = "all", string orderByType = "date",
+            int startPrice = 0, int endPrice = 0, List<int> selectedGroups = null)
+        {
+            ViewBag.selectedGroups = selectedGroups;
+            ViewBag.Groups = _courseService.GetAllGroup();
+            ViewBag.pageId = pageId;
+            return View(_courseService.GetCourse(pageId,filter,getType,orderByType,startPrice,endPrice,selectedGroups,9));
+        }
+
+
+        [Route("ShowCourse/{id}")]
+        public IActionResult ShowCourse(int id)
+        {
+            var course = _courseService.GetCourseForShow(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        [Authorize]
+        public ActionResult BuyCourse(int id)
+        {
+           int orderId= _orderService.AddOrder(User.Identity.Name, id);
+            return Redirect("/UserPanel/MyOrders/ShowOrder/" + orderId);
+        }
+
+        [Route("DownloadFile/{episodeId}")]
+        public IActionResult DownloadFile(int episodeId)
+        {
+            var episode = _courseService.GetEpisodeById(episodeId);
+            string filepath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/courseFiles",
+                episode.EpisodeFileName);
+            string fileName = episode.EpisodeFileName;
+            if (episode.IsFree)
+            {
+                byte[] file = System.IO.File.ReadAllBytes(filepath);
+                return File(file, "application/force-download", fileName);
+            }
+
+            if (User.Identity.IsAuthenticated)
+            {
+                if (_orderService.IsUserInCourse(User.Identity.Name, episode.CourseId))
+                {
+                    byte[] file = System.IO.File.ReadAllBytes(filepath);
+                    return File(file, "application/force-download", fileName);
+                }
+            }
+
+            return Forbid();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult CreateComment(CourseComment comment)
+        {
+            comment.IsDelete = false;
+            comment.CreateDate=DateTime.Now;
+            comment.UserId = _userService.GetUserIdByUserName(User.Identity.Name);
+            _courseService.AddComment(comment);
+
+            return View("ShowComment",_courseService.GetCourseComment(comment.CourseId));
+        }
+
+        public IActionResult ShowComment(int id,int pageId=1)
+        {
+            return View(_courseService.GetCourseComment(id, pageId));
+        }
+    }
+}
--- MyCvProject.UI/Startup.cs
+++ MyCvProject.UI/Startup.cs
@@ -1,39 +1,104 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TopLearn.Core.Convertors;
+using TopLearn.Core.Services;
+using TopLearn.Core.Services.Interfaces;
+using TopLearn.DataLayer.Context;
 
-namespace MyCvProject.UI
+namespace TopLearn.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            //services.Configure<FormOptions>(options => { options.MultipartBodyLengthLimit = 6000000; });
+        
+
+
+            #region Authentication
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme= CookieAuthenticationDefaults.AuthenticationScheme;
+
+            }).AddCookie(options =>
+            {
+                options.LoginPath = "/Login";
+                options.LogoutPath = "/Logout";
+                options.ExpireTimeSpan=TimeSpan.FromMinutes(43200);
+
+            });
+
+            #endregion
+
+            #region DataBase Context
+
+            services.AddDbContext<TopLearnContext>(options =>
+                {
+                    options.UseSqlServer(Configuration.GetConnectionString("TopLearnConnection"));
+                }
+            );
+
+            #endregion
+
+            #region IoC
+
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IViewRenderService, RenderViewToString>();
+            services.AddTransient<IPermissionService, PermissionService>();
+            services.AddTransient<ICourseService, CourseService>();
+            services.AddTransient<IOrderService, OrderService>();
+
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            app.UseStaticFiles();
+            app.UseAuthentication();
+         
+            app.UseMvc(routes =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                    
+                );
+                routes.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
             });
         }
     }
